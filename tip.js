a86273f316f7e02e4cd70459a208049456437955
adrianh42 <adrianhartanto46@gmail.com>
2022-01-01 21:20:34 +0700

    fourth-commit

--- tip.js
+++ tip.js
@@ -1,66 +1,122 @@
+// Functions
+
 const clear = function () {
   this.value = "";
 };
 
-const tipCalc = function () {
-  tip = ((percent / 100) * bill) / person;
-  tipAmount.innerHTML = tip.toFixed(2);
+const calc = function () {
+  if (bill == 0 || percent == 0 || person == 0) {
+    tipAmount.innerHTML = 0;
+    totalAmount.innerHTML = 0;
+  } else {
+    tip = ((percent / 100) * bill) / person;
+    tipAmount.innerHTML = tip.toFixed(2);
+    total = bill / person + tip;
+    totalAmount.innerHTML = total.toFixed(2);
+  }
 };
 
-const totalCalc = function () {
-  total = bill / person + tip;
-  totalAmount.innerHTML = total.toFixed(2);
+const resetSelected = function () {
+  for (button of percentButton) {
+    button.classList.remove("active");
+  }
 };
 
-const setDefault = function () {};
+const isDefault = function () {
+  if (percent == 0) {
+    percentButton[1].classList.toggle("active");
+    percent = 10;
+  }
+  if (person == 0) {
+    numPeople.value = 1;
+    person = 1;
+  }
+};
 
-const billAmount = document.querySelector("#bill");
+// Code
 
-const percentButton = document.querySelectorAll(".percent");
-
-for (button of percentButton) {
-  console.log(button.innerHTML);
-}
+let percent = 0;
 
 let bill = 0;
+
+let person = 0;
+
+// Bill amount
+
+const billAmount = document.querySelector("#bill");
 
 billAmount.addEventListener("change", function () {
   bill = this.value;
   console.log(this.value);
-  tipCalc();
-  totalCalc();
+  isDefault();
+  calc();
 });
 
+// Percentage buttons
+
+const percentButton = document.querySelectorAll(".percent");
+
+for (button of percentButton) {
+  button.addEventListener("click", function () {
+    resetSelected();
+    this.classList.toggle("active");
+    percent = this.innerHTML.slice(0, -1);
+    customPercent.value = "";
+    isDefault();
+    calc();
+  });
+}
+
+// Custom percentage
+
 const customPercent = document.querySelector("#custom-percent");
-
-let percent = 10;
-
-const click = () => {
-  console.log("I was clicked!");
-};
 
 customPercent.addEventListener("click", clear);
 
 customPercent.addEventListener("change", function () {
+  resetSelected();
   percent = this.value;
   console.log(this.value);
   this.value += "%";
-  tipCalc();
-  totalCalc();
+  isDefault();
+  calc();
 });
+
+// Number of people
 
 const numPeople = document.querySelector("#people-amount");
 
-let person = 1;
+numPeople.addEventListener("click", clear);
 
 numPeople.addEventListener("change", function () {
+  const error = document.querySelector(".error-text");
+  if (numPeople.value == 0) {
+    error.classList.toggle("hidden");
+  } else {
+    error.classList.add("hidden");
+  }
   person = this.value;
   console.log(this.value);
-  tipCalc();
-  totalCalc();
+  isDefault();
+  calc();
 });
 
 const tipAmount = document.querySelector("#tip");
 let tip = 0;
 const totalAmount = document.querySelector("#total");
 let total = 0;
+
+// Reset button
+
+const resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function () {
+  resetSelected();
+  numPeople.value = "";
+  customPercent.value = "";
+  billAmount.value = "";
+  percent = 0;
+  bill = 0;
+  person = 0;
+  calc();
+});
